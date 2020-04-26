06c1ac1282a44b51c59ccdd5d058d481a8907cc0
vivek sahani <54721640+SilenMe@users.noreply.github.com>
2020-04-26 06:17:03 +0530

    Add files via upload

---
+++ take a break/braek_time.py
@@ -0,0 +1,6 @@
+import webbrowser
+import time
+print("this program started on"+time.ctime())
+for i in range (0,3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=e-1kNWpjHqw")
